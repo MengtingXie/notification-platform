34ad8d1d8aa9cbdee8c77f75e235c13b8272e54b
Deng Ming <ming_flycash@qq.com>
2025-03-27 11:22:22 +0800

    暴露config对象和service

--- internal/service/config/moudle.go
+++ internal/service/config/moudle.go
@@ -1,10 +1,15 @@
 package config
 
 import (
+	"gitee.com/flycash/notification-platform/internal/service/config/internal/domain"
 	"gitee.com/flycash/notification-platform/internal/service/config/internal/service"
 )
 
-type Module struct {
-	ignoredInitTablesErr error
-	Svc                  service.BusinessConfigService
-}
+type (
+	Module struct {
+		ignoredInitTablesErr error
+		Svc                  service.BusinessConfigService
+	}
+	BusinessConfig = domain.BusinessConfig
+	Service = service.BusinessConfigService
+)
