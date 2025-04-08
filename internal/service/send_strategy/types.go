e305b685efa5f7257604db3581e98239b5cb40cc
Deng Ming <ming_flycash@qq.com>
2025-04-08 17:19:19 +0800

    删除send_strategy中的方法

--- internal/service/send_strategy/types.go
+++ internal/service/send_strategy/types.go
@@ -12,7 +12,6 @@ import (
 type SendStrategy interface {
 	// Send 单条发送通知
 	Send(ctx context.Context, notification domain.Notification) (domain.SendResponse, error)
-	Commit(ctx context.Context, notification domain.Notification) (domain.SendResponse, error)
 	// BatchSend 批量发送通知，其中每个通知的发送策略必须相同
 	BatchSend(ctx context.Context, notifications []domain.Notification) ([]domain.SendResponse, error)
 }
