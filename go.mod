74cb2b50c7a606686fbeaa5be56b43a770d1763c
Deng Ming <ming_flycash@qq.com>
2025-04-19 23:56:36 +0800

    完成服务注册与服务发现模块及其测试

--- go.mod
+++ go.mod
@@ -27,11 +27,14 @@ require (
 	github.com/stretchr/testify v1.10.0
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.1134
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.1115
+	go.etcd.io/etcd/api/v3 v3.5.20
+	go.etcd.io/etcd/client/v3 v3.5.20
 	go.opentelemetry.io/otel v1.35.0
 	go.opentelemetry.io/otel/exporters/zipkin v1.19.0
 	go.opentelemetry.io/otel/sdk v1.34.0
 	go.opentelemetry.io/otel/trace v1.35.0
 	go.uber.org/mock v0.3.0
+	golang.org/x/net v0.38.0
 	golang.org/x/sync v0.12.0
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.5
@@ -130,9 +133,7 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.45.0 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.20 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.20 // indirect
-	go.etcd.io/etcd/client/v3 v3.5.20 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/jaeger v1.17.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.18.0 // indirect
@@ -145,7 +146,6 @@ require (
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250106144421-5f5ef82da422 // indirect
---
+++ internal/pkg/client/grpc_resolver.go
@@ -0,0 +1,96 @@
+package client
+
+import (
+	"time"
+
+	"gitee.com/flycash/notification-platform/internal/pkg/registry"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/attributes"
+	"google.golang.org/grpc/resolver"
+)
+
+type grpcResolverBuilder struct {
+	r       registry.Registry
+	timeout time.Duration
+}
+
+func NewResolverBuilder(r registry.Registry, timeout time.Duration) resolver.Builder {
+	return &grpcResolverBuilder{
+		r:       r,
+		timeout: timeout,
+	}
+}
+
+func (r *grpcResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (resolver.Resolver, error) {
+	res := &grpcResolver{
+		target:   target,
+		cc:       cc,
+		registry: r.r,
+		close:    make(chan struct{}, 1),
+		timeout:  r.timeout,
+	}
+	res.resolve()
+	go res.watch()
+	return res, nil
+}
+
+func (r *grpcResolverBuilder) Scheme() string {
+	return "registry"
+}
+
+type grpcResolver struct {
+	target   resolver.Target
+	cc       resolver.ClientConn
+	registry registry.Registry
+	close    chan struct{}
+	timeout  time.Duration
+}
+
+func (g *grpcResolver) ResolveNow(_ resolver.ResolveNowOptions) {
+	// 重新获取一下所有服务
+	g.resolve()
+}
+
+func (g *grpcResolver) Close() {
+	g.close <- struct{}{}
+}
+
+func (g *grpcResolver) watch() {
+	events := g.registry.Subscribe(g.target.Endpoint())
+	for {
+		select {
+		case <-events:
+			g.resolve()
+
+		case <-g.close:
+			return
+		}
+	}
+}
+
+func (g *grpcResolver) resolve() {
+	serviceName := g.target.Endpoint()
+	ctx, cancel := context.WithTimeout(context.Background(), g.timeout)
+	instances, err := g.registry.ListServices(ctx, serviceName)
+	cancel()
+	if err != nil {
+		g.cc.ReportError(err)
+	}
+
+	address := make([]resolver.Address, 0, len(instances))
+	for _, ins := range instances {
+		address = append(address, resolver.Address{
+			Addr:       ins.Address,
+			ServerName: ins.Name,
+			Attributes: attributes.New(readWeightStr, ins.ReadWeight).
+				WithValue(writeWeightStr, ins.WriteWeight).
+				WithValue(groupStr, ins.Group),
+		})
+	}
+	err = g.cc.UpdateState(resolver.State{
+		Addresses: address,
+	})
+	if err != nil {
+		g.cc.ReportError(err)
+	}
+}
