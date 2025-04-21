a69f25f46f52e0e6e05887330750d0b724962c84
Deng Ming <ming_flycash@qq.com>
2025-04-21 20:53:49 +0800

    4.20 答疑

---
+++ internal/demo/registry/multiple_registry.go
@@ -0,0 +1,29 @@
+package registry
+
+import "context"
+
+type MultipleRegistry struct {
+	registries []Registry
+}
+
+func (m *MultipleRegistry) Register(ctx context.Context, svc Service) error {
+	for _, r := range m.registries {
+		// 直接中断。没必要再去 Deregister 已经注册成功的
+		err := r.Register(ctx, svc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *MultipleRegistry) Deregister(ctx context.Context, svc Service) error {
+	// 尽可能完成的
+	for _, r := range m.registries {
+		err := r.Deregister(ctx, svc)
+		if err != nil {
+			// 记录日志
+		}
+	}
+	return nil
+}
