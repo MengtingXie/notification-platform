df8f6cb80b24d560d6dac06bddbf88c4d8965c41
Deng Ming <ming_flycash@qq.com>
2025-04-22 11:49:27 +0800

    添加链路超时

---
+++ internal/pkg/client/timeout/interceptor.go
@@ -0,0 +1,31 @@
+package timeout
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+const timeoutStr = "timeout"
+
+// TimeoutInjectorInterceptor 超时时间注入拦截器
+func TimeoutInjectorInterceptor() grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		// 1. 从context提取超时时间
+		if deadline, ok := ctx.Deadline(); ok {
+			// 3. 注入metadata（兼容已有metadata）
+			ctx = metadata.AppendToOutgoingContext(ctx, timeoutStr, fmt.Sprintf("%d", deadline.UnixMilli()))
+		}
+		// 4. 透传修改后的context
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
