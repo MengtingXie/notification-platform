11f6be0d6713ab18bf086a759c869beff5f64955
Deng Ming <ming_flycash@qq.com>
2025-04-24 14:56:07 +0800

    负载均衡演示

--- internal/demo/registry/multiple_registry.go
+++ internal/demo/registry/multiple_registry.go
@@ -1,14 +1,44 @@
 package registry
 
-import "context"
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+
+	"github.com/hashicorp/go-multierror"
+)
 
 type MultipleRegistry struct {
 	registries []Registry
+	main       RegistryStatus
+	back       RegistryStatus
+}
+
+// RegistryStatus 增加心跳方法，然后心跳不通，就把 active 设置为 false
+type RegistryStatus struct {
+	r      Registry
+	active atomic.Bool
+}
+
+func (m *MultipleRegistry) ResolveV1(ctx context.Context, svcName string) ([]Service, error) {
+	if m.main.active.Load() {
+		return m.main.r.Resolve(ctx, svcName)
+	}
+	return m.back.r.Resolve(ctx, svcName)
+}
+
+func (m *MultipleRegistry) Resolve(ctx context.Context, svcName string) ([]Service, error) {
+	for _, r := range m.registries {
+		svcs, err := r.Resolve(ctx, svcName)
+		if err == nil {
+			return svcs, nil
+		}
+	}
+	return nil, errors.New("所有注册中心都失败了")
 }
 
 func (m *MultipleRegistry) Register(ctx context.Context, svc Service) error {
 	for _, r := range m.registries {
-		// 直接中断。没必要再去 Deregister 已经注册成功的
 		err := r.Register(ctx, svc)
 		if err != nil {
 			return err
@@ -18,12 +48,15 @@ func (m *MultipleRegistry) Register(ctx context.Context, svc Service) error {
 }
 
 func (m *MultipleRegistry) Deregister(ctx context.Context, svc Service) error {
-	// 尽可能完成的
+	var err error
 	for _, r := range m.registries {
-		err := r.Deregister(ctx, svc)
-		if err != nil {
-			// 记录日志
-		}
+		err = multierror.Append(err, r.Deregister(ctx, svc))
+		// 你可以考虑记录日志，Deregister != nil 的时候
 	}
-	return nil
+	return err
+}
+
+type MultipleRegistryV1 struct {
+	// mainClient   *etcdv3.Client
+	// backupClient *etcdv3.Client
 }
--- internal/demo/registry/types.go
+++ internal/demo/registry/types.go
@@ -5,6 +5,7 @@ import "context"
 type Registry interface {
 	Register(ctx context.Context, svc Service) error
 	Deregister(ctx context.Context, svc Service) error
+	Resolve(ctx context.Context, svcName string) ([]Service, error)
 }
 
 type Service struct {
