0faea36f8e5236ac8af78fe673d76744398b5adf
Deng Ming <ming_flycash@qq.com>
2025-05-05 18:20:06 +0800

    修复 wire 中的问题

--- internal/test/ioc/id.go
+++ internal/test/ioc/id.go
@@ -6,6 +6,8 @@ import (
 	"github.com/sony/sonyflake"
 )
 
+// 在引入自定义分库分表算法之前临时用的 ID 生成算法
+// InitIDGenerator
 func InitIDGenerator() *sonyflake.Sonyflake {
 	// 使用固定设置的ID生成器
 	return sonyflake.NewSonyflake(sonyflake.Settings{
--- internal/test/ioc/set.go
+++ internal/test/ioc/set.go
@@ -2,4 +2,7 @@ package ioc
 
 import "github.com/google/wire"
 
-var BaseSet = wire.NewSet(InitDBAndTables, InitProviderEncryptKey, InitCache, InitMQ, InitIDGenerator, InitRedis, InitRedisClient, InitDistributedLock)
+var BaseSet = wire.NewSet(InitDBAndTables, InitProviderEncryptKey, InitCache, InitMQ, InitRedis, InitRedisClient, InitDistributedLock)
+
+// 在你还没有引入自己定义的 ID 生成算法之前，你用下面这个
+// var BaseSet = wire.NewSet(InitDBAndTables, InitProviderEncryptKey, InitCache, InitMQ, InitIDGenerator, InitRedis, InitRedisClient, InitDistributedLock)
